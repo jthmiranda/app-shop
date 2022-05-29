540ad1f6737d5c752ae18bc1a5a3d98c0eb494bf
Jonathan Miranda <miranda.jth@gmail.com>
2022-05-29 12:25:37 -0600

    Creating Product factory and seeder
    note: the seeder can be avoided just declaring create on DatabaseSeeder.php

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'description' => $this->faker->sentence(10),
+            'long_description' => $this->faker->text,
+            'price' => $this->faker->randomFloat(2, 5, 150)
+        ];
+    }
+}
---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // model factories
+        Product::factory(100)->create();
+    }
+}
