256eb8cae0373a0383aaadc0a1eb552350a32733
Jonathan Miranda <miranda.jth@gmail.com>
2022-05-29 09:43:04 -0600

    Adding a new template from Material Kit v1.1
    pages modified until now
    [+] welcome.blade.php
    [+] register.blade.php
    [+] login.blade.php
    [+] home.blade.php
    [+] app.blade.php
    
    Fully functional login and register new users

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,101 @@
 @extends('layouts.app')
 
+@section('body-class', 'signup-page')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+    <div class="header header-filter" style="background-image: url('{{ asset('img/city.jpg') }}'); background-size: cover; background-position: top center;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+                    <div class="card card-signup">
+                        <form class="form" method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="header header-primary text-center">
+                                <h4>Inicio de sesión</h4>
+{{--                                <div class="social-line">--}}
+{{--                                    <a href="#pablo" class="btn btn-simple btn-just-icon">--}}
+{{--                                        <i class="fa fa-facebook-square"></i>--}}
+{{--                                    </a>--}}
+{{--                                    <a href="#pablo" class="btn btn-simple btn-just-icon">--}}
+{{--                                        <i class="fa fa-twitter"></i>--}}
+{{--                                    </a>--}}
+{{--                                    <a href="#pablo" class="btn btn-simple btn-just-icon">--}}
+{{--                                        <i class="fa fa-google-plus"></i>--}}
+{{--                                    </a>--}}
+{{--                                </div>--}}
                             </div>
-                        </div>
+                            <p class="text-divider">Ingresa tus datos</p>
+                            <div class="content">
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                                <div class="input-group">
+										<span class="input-group-addon">
+											<i class="material-icons">email</i>
+										</span>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                    <input id="email" placeholder="Email..." type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                </div>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                                <div class="input-group">
+										<span class="input-group-addon">
+											<i class="material-icons">lock_outline</i>
+										</span>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <input id="password" placeholder="Password..." type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                </div>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                <!-- If you want to add a checkbox to this form, uncomment this code-->
+
+                                <div class="checkbox">
+                                    <label>
+
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                        Recordad sesión
                                     </label>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                            <div class="footer text-center">
+                                <button type="submit" class="btn btn-simple btn-primary btn-lg">Ingresar</button>
                             </div>
+{{--                            <a class="btn btn-link" href="{{ route('password.request') }}">--}}
+{{--                                {{ __('Forgot Your Password?') }}--}}
+{{--                            </a>--}}
+
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
+
+            <footer class="footer">
+                <div class="container">
+                    <nav class="pull-left">
+                        <ul>
+                            <li>
+                                <a href="http://www.creative-tim.com">
+                                    Creative Tim
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://presentation.creative-tim.com">
+                                    About Us
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://blog.creative-tim.com">
+                                    Blog
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.creative-tim.com/license">
+                                    Licenses
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div class="copyright pull-right">
+                        &copy; 2016, made with <i class="fa fa-heart heart"></i> by <a href="http://www.creative-tim.com" target="_blank">Creative Tim</a>
+                    </div>
+                </div>
+            </footer>
+
         </div>
-    </div>
-</div>
-@endsection
+    @endsection
