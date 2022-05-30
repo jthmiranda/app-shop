2808217bd1f61c86653b21679ed4bd5d1fb1141b
Jonathan Miranda <miranda.jth@gmail.com>
2022-05-30 14:41:08 -0600

    applying validation to the form edit products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -54,6 +54,22 @@ class ProductController extends Controller
     }
 
     public function update(Request $request, $id) {
+        //validar
+        $messages = [
+            'name.required' => 'Es necesario ingresar un nombre para el producto',
+            'name.min' => 'El nombre del producto debe tener al menos 3 caracteres',
+            'description.required' => 'La descripcion corta es un campo obligatorio',
+            'description.min' => 'La descripcion corta solo admite hasta 200 caracteres',
+            'price.required' => 'Es obligatorio definir un precio para el producto',
+            'price.min' => 'No se admiten valores negativos'
+        ];
+        $rules = [
+            'name' => 'required|min:3',
+            'description' => 'required|max:200',
+            'price' => 'required|numeric|min:0'
+        ];
+        $this->validate($request, $rules, $messages);
+
         // funcion para persistir datos en mysql
         //dd($request->all());
         $product = Product::find($id);
