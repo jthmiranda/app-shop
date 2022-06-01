37d5b93ff402362ce91f58e465441861eeab1fbf
Jonathan Miranda <miranda.jth@gmail.com>
2022-06-01 15:36:25 -0600

    creating model and migrations for Cart and CartDetails

---
+++ database/migrations/2022_06_01_204527_create_carts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->date('order_date');
+            $table->date('arrived_date');
+            $table->string('status'); // Active, Pending, Approved, Cancelled, Finished
+
+            // user_id (FK) customer
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
