8aff1ff46b0584ed53347c6b90e653efcbf3cb6c
Jonathan Miranda <miranda.jth@gmail.com>
2022-05-31 21:53:15 -0600

    applying @include directive for footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,31 @@
+<footer class="footer">
+    <div class="container">
+        <nav class="pull-left">
+            <ul>
+                <li>
+                    <a href="http://www.creative-tim.com">
+                        Necesitas ayuda con tu proyecto?
+                    </a>
+                </li>
+                <li>
+                    <a href="http://presentation.creative-tim.com">
+                        Asesoría personalizada
+                    </a>
+                </li>
+                <li>
+                    <a href="http://blog.creative-tim.com">
+                        Blog
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.creative-tim.com/license">
+                        Tutoriales sobre desarrollo web y móvil
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright pull-right">
+            &copy; 2022, Material Kit gracias a <i class="fa fa-heart heart"></i> by Creative Tim
+        </div>
+    </div>
+</footer>
