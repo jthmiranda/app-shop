7292e31aa45d7422206939b5bf0e2305ffcbec7a
Jonathan Miranda <miranda.jth@gmail.com>
2022-06-02 20:58:31 -0600

    creating profile product page to see details about

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Models\Product;
 
 class ProductController extends Controller
 {
     public function show($id) {
-        dd($id);
-//        return view('');
+        $product = Product::find($id);
+        $images = $product->images;
+
+        $imagesLeft = collect();
+        $imagesRight = collect();
+
+        foreach ($images as $key => $image) {
+            if ($key%2==0) {
+                $imagesLeft->push($image);
+            } else {
+                $imagesRight->push($image);
+            }
+        }
+        return view('products.show')->with(compact('product', 'imagesLeft', 'imagesRight'));
     }
 }
