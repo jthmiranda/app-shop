b43b03c7170f9ac05bb370868586d121711ba6a0
Jonathan Miranda <miranda.jth@gmail.com>
2022-06-03 20:39:58 -0600

    setting onetomany in Cart model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+
+    public function details() {
+        return $this->hasMany(CartDetail::class);
+    }
 }
